375349771311c7aab37f4b91c8e7e28bb01bf0e4
xbiwas <biwas2059@gmail.com>
2024-05-29 17:54:56 +0545

    Python OOP

---
+++ oop/04.py
@@ -0,0 +1,27 @@
+# Encapsulation
+
+class Car:
+    def __init__(self, brand, model):
+        self.__brand = brand
+        self.model = model
+        
+    # Encapsulation is done here
+    def get_brand(self):
+        return self.__brand + "!"
+
+    def full_name(self):
+        return f"{self.__brand} {self.model}"
+
+
+# my_car = Car("TATA", "Safari")
+# print(my_car.full_name())
+# print(my_car.get_brand())
+class ElectricCar(Car):
+    def __init__(self, __brand, model, batterySize):
+        super().__init__(__brand, model)
+        self.batterySize = batterySize
+
+my_electric_car = ElectricCar("TSLA", "Model S", "85KWH")
+# print(my_electric_car.full_name())
+# print(my_electric_car.__brand)
+print(my_electric_car.get_brand())

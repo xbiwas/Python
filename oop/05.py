d7bbe6e412e3767bf4f0c262cdbd11ef5dbc0a5a
xbiwas <biwas2059@gmail.com>
2024-05-29 18:07:52 +0545

    Python OOP Polymorphism

---
+++ oop/05.py
@@ -0,0 +1,32 @@
+class Car:
+    def __init__(self, brand, model):
+        self.__brand = brand
+        self.model = model
+    
+    def get_brand(self):
+        return self.__brand
+
+    def full_name(self):
+        return f"{self.__brand} {self.model}"
+    
+    def fuel_type(self):
+        return "Petrol or Diesel"
+
+myCar = Car("TATA", "Safari")
+# print(myCar.get_brand())
+# print(myCar.full_name())
+
+class ElectricCar(Car):
+    def __init__(self, __brand, model, batterySize):
+        self.batterySize = batterySize
+        super().__init__(__brand, model)
+    
+    def fuel_type(self):
+        return "Electric Charge"
+
+myElectricCar = ElectricCar("TSLA", "Model S", "85KWH")
+# print(myElectricCar.get_brand())
+# print(myElectricCar.full_name())
+
+print(myCar.fuel_type())
+print(myElectricCar.fuel_type())
